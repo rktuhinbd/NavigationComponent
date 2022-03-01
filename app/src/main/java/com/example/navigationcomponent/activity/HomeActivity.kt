cf1001dbb52664a43e24827fd67f10a4bb288f6f
Md Rejaul Karim <tuhin@10minuteschool.com>
2022-03-01 17:54:27 +0600

    Text added in Home Activity

--- app/src/main/java/com/example/navigationcomponent/activity/HomeActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/activity/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.example.navigationcomponent.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.navigationcomponent.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.navigationcomponent.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/example/navigationcomponent/activity/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/activity/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.navigationcomponent.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.navigationcomponent.R
+import com.example.navigationcomponent.databinding.ActivityMainBinding
+import com.example.navigationcomponent.databinding.FragmentGetStartedBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
     }
 }
