7e81881c3f6116ef9618ba81ffe973ea32ca90ba
Md Rejaul Karim <tuhin@10minuteschool.com>
2022-03-01 16:54:55 +0600

    Project gradle downgraded to made compatible with old IDEs

---
+++ app/src/main/java/com/example/navigationcomponent/fragment/GetStartedFragment.kt
@@ -0,0 +1,27 @@
+package com.example.navigationcomponent.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponent.R
+import com.example.navigationcomponent.databinding.FragmentGetStartedBinding
+import com.example.navigationcomponent.databinding.FragmentSignInBinding
+
+class GetStartedFragment : Fragment() {
+
+    private lateinit var binding : FragmentGetStartedBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentGetStartedBinding.inflate(inflater, container, false)
+
+        binding.btnSignIn.setOnClickListener {
+//            val navAction = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
+//            findNavController().navigate(navAction)
+        }
+
+        return binding.root
+    }
+}
